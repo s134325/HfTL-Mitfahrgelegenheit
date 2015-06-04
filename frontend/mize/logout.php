b263fefbc4b13a290e2432676f481e5546b89dfe
s134329 <s134329@hft-leipzig.de>
2015-06-04 19:02:52 +0200

    Revert "frontend"
    This reverts commit f864eeb479942467ffcc6b7d9a0cda4eabadbf4f.

--- frontend/mize/logout.php
+++
@@ -1,9 +0,0 @@
----logout.php---
-<?php
-// erstmal ne Session starten, damit wir sie auch löschen können
-session_start();
-// Die Besagte löschen
-session_destroy();
-header ("Location: login.html");
-// und zurück zu login.html
-?>
